575313649eea1dfec45b0f6712fa05fed2064dda
Swaraj SIngh Chauhan <swarajsingh.chauhan@research.iiit.ac.in>
2018-03-29 13:07:28 +0530

    Template

---
+++ 16_consumer_CSAProfile.php
@@ -0,0 +1,335 @@
+<!DOCTYPE html>
+<html>
+<head>
+<?php require 'essential.inc.php';
+    require 'functions.php';
+?>
+
+  <!-- Site made with Mobirise Website Builder v4.3.0, https://mobirise.com -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v4.3.0, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="shortcut icon" href="assets/images/mbr-favicon.png" type="image/x-icon">
+  <meta name="description" content="Site Maker Description">
+  <title>23_profileFPO</title>
+  <link rel="stylesheet" href="assets/bootstrap-material-design-font/css/material.css">
+  <link rel="stylesheet" href="assets/et-line-font-plugin/style.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i">
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic&subset=latin">
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
+  <link rel="stylesheet" href="assets/soundcloud-plugin/style.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/dropdown/css/style.css">
+  <link rel="stylesheet" href="assets/animate.css/animate.min.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+  
+  
+  
+</head>
+<body>
+<section id="menu-h" data-rv-view="120">
+
+    <nav class="navbar navbar-dropdown transparent navbar-fixed-top bg-color">
+        <div class="container">
+
+            <div class="mbr-table">
+                <div class="mbr-table-cell">
+
+                    <div class="navbar-brand">
+                        <a href="14_consumerBuy.php" class="mbri-globe mbr-iconfont mbr-iconfont-menu navbar-logo"></a>
+                        <a class="navbar-caption" href="14_consumerBuy.php">Fresh Farm</a>
+                    </div>
+
+                </div>
+                <div class="mbr-table-cell">
+
+                    <button class="navbar-toggler pull-xs-right hidden-md-up" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar">
+                        <div class="hamburger-icon"></div>
+                    </button>
+
+                    <ul class="nav-dropdown collapse pull-xs-right nav navbar-nav navbar-toggleable-sm" id="exCollapsingNavbar"><li class="nav-item"><a class="nav-link link" data-toggle="modal" data-target="#login" href="#">Bid</a></li><li class="nav-item"><a class="nav-link link" href="13_consumerShareDisplay.php">CSA</a></li><li class="nav-item "><a class="nav-link link " href="14_consumerBuy.php">Buy</a><li class="nav-item "><a class="nav-link link" href="cart.php" >Cart</a></li><li class="nav-item dropdown"><a class="nav-link link dropdown-toggle" data-toggle="dropdown-submenu" href="#" aria-expanded="false">Account</a><div class="dropdown-menu"><a class="dropdown-item" href="profile.php">Profile</a><a class="dropdown-item" href="12_consumerMap.php">Connect More</a><a class="dropdown-item" href="logout.php">logout</a></div></li></ul>
+                    <button hidden="" class="navbar-toggler navbar-close" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar">
+                        <div class="close-icon"></div>
+                    </button>
+
+                </div>
+            </div>
+
+        </div>
+    </nav>
+
+</section>
+
+<section class="engine"><a href="https://mobirise.co/o">bootstrap button</a></section><section class="mbr-section mbr-section-hero mbr-section-full header2 mbr-parallax-background mbr-after-navbar" id="header6-25" data-rv-view="7" style="background-image: url(assets/images/room-laptop.jpg);">
+
+    <div class="mbr-overlay" style="opacity: 0.8; background-color: rgb(204, 204, 204);">
+    </div>
+
+    <div class="mbr-table mbr-table-full">
+        <div class="mbr-table-cell">
+
+            <div class="container">
+                <div class="mbr-section row">
+                    <div class="mbr-table-md-up">
+                        
+                        <div class="mbr-table-cell mbr-right-padding-md-up mbr-valign-top col-md-7 image-size" style="width: 50%;">
+                            <div class="mbr-figure"><img  src="farm.jpg" width="200" height="400" ></div>
+                        </div>
+                        <div class="mbr-table-cell col-md-5 text-xs-center text-md-left content-size">
+                        <?php
+                           getHeader();
+                        ?>
+
+                            
+
+                        </div>
+
+                        
+                        
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="mbr-arrow mbr-arrow-floating hidden-sm-down" aria-hidden="true"><a href="#features7-24"><i class="mbr-arrow-icon"></i></a></div>
+
+</section>
+<?php 
+   // include 'BuyCSA.inc.php';
+?>
+
+<section class="mbr-cards mbr-section mbr-section-nopadding" id="features7-24" data-rv-view="14" style="background-color: rgb(239, 239, 239);">
+
+    
+
+    <div class="mbr-cards-row row">
+        <div class="mbr-cards-col col-xs-12 col-lg-4" style="padding-top: 80px; padding-bottom: 80px;">
+            <div class="container">
+                <div class="card cart-block">
+                    <div class="card-img iconbox"><a href="https://mobirise.com" class="etl-icon icon-phone mbr-iconfont mbr-iconfont-features7" style="color: rgb(255, 255, 255);"></a></div>
+                    <div class="card-block">
+                        <h4 class="card-title">3 months</h4>
+                        
+                        <p class="card-text">
+
+<b>Season:</b>  January through January<br>
+
+<b>Type:</b>  3rd party CSA service<br>
+
+<b>Since:</b>  2010<br>
+
+<b># of Shares:</b>  150<br>
+
+<b>Full Share:</b>  $325/15 wks. or $450/15 wks.<br>
+
+<b>Work Req? :</b>  No<br>
+</p>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="mbr-cards-col col-xs-12 col-lg-4" style="padding-top: 80px; padding-bottom: 80px;">
+            <div class="container">
+                <div class="card cart-block">
+                    <div class="card-img iconbox"><a href="https://mobirise.com" class="etl-icon icon-edit mbr-iconfont mbr-iconfont-features7" style="color: rgb(255, 255, 255);"></a></div>
+                    <div class="card-block">
+                        <h4 class="card-title">6 months</h4>
+                        
+                        <p class="card-text">
+
+<b>Season:</b>  January through January<br>
+
+<b>Type:</b>  3rd party CSA service<br>
+
+<b>Since:</b>  2010<br>
+
+<b># of Shares:</b>  150<br>
+
+<b>Full Share:</b>  $325/15 wks. or $450/15 wks.<br>
+
+<b>Work Req? :</b>  No<br>
+</p>
+                        
+                    </div>
+                </div>
+          </div>
+        </div>
+        <div class="mbr-cards-col col-xs-12 col-lg-4" style="padding-top: 80px; padding-bottom: 80px;">
+            <div class="container">
+                <div class="card cart-block">
+                    <div class="card-img iconbox"><a href="https://mobirise.com" class="etl-icon icon-briefcase mbr-iconfont mbr-iconfont-features7" style="color: rgb(255, 255, 255);"></a></div>
+                    <div class="card-block">
+                        <h4 class="card-title">12 months</h4>
+                        
+                        <p class="card-text">
+
+<b>Season:</b>  January through January<br>
+
+<b>Type:</b>  3rd party CSA service<br>
+
+<b>Since:</b>  2010<br>
+
+<b># of Shares:</b>  150<br>
+
+<b>Full Share:</b>  $325/15 wks. or $450/15 wks.<br>
+
+<b>Work Req? :</b>  No<br>
+</p>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        
+        
+    </div>
+</section>
+
+<section class="mbr-section article mbr-section__container" id="content2-2e" data-rv-view="198" style="background-color: rgb(255, 255, 255); padding-top: 20px; padding-bottom: 20px;">
+
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 lead"><blockquote>
+
+            	<p>
+            		<center><b>Delivery adderesses</b></center><br>
+            		<ul>
+            			<li>
+            				<b>K-Mart Parking Lot</b>
+            &nbsp;(Tue)<br>
+            Members pick their shares up on Tuesday afternoons from 5:30-6:00.
+            
+        </p>
+
+        <p>
+            
+            <b>Contact:</b> Andrew Strickland<br>
+            
+            
+            <b>Phone:</b> 252-230-0345<br>
+            
+            
+            <b>Address:</b><br>
+            720 Sutters Creek Blvd. <br>
+            Rocky Mount, NC 27804
+
+            
+
+        </p>
+        </li>
+<br>
+
+        <li>
+            				<b>Nashville Agriculture Center</b>
+            &nbsp;(Fri)<br>
+            
+            Members pick up their shares on Tuesday afternoons from 4:45-5:15.
+            
+        </p>
+
+        <p>
+            
+            <b>Contact:</b> Andrew Strickland<br>
+            
+            
+            <b>Phone:</b> 252-230-0345<br>
+            
+            
+            <b>Address:</b><br>
+            1006 Eastern Avenue<br>
+            Nashville, NC 27856
+
+            
+
+        </p>
+        </li>
+            		</ul>
+
+            	</p>
+
+            </blockquote></div>
+
+        </div>
+    </div>
+
+</section>
+
+
+<section class="mbr-section mbr-parallax-background" id="testimonials4-1i" data-rv-view="17" style="background-image: url(assets/images/sky.jpg); padding-top: 120px; padding-bottom: 120px;">
+
+    <div class="mbr-overlay" style="opacity: 0.2; background-color: rgb(34, 34, 34);">
+    </div>
+
+        <div class="mbr-section mbr-section__container mbr-section__container--middle">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12 text-xs-center">
+                        <h3 class="mbr-section-title display-2">WHAT OUR FANTASTIC USERS SAY</h3>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    <div class="mbr-section mbr-section-nopadding">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-xs-12">
+                <?php
+                    //getReviews();
+                ?>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<section class="mbr-section mbr-section-md-padding mbr-footer footer2" id="contacts2-1j" data-rv-view="27" style="background-color: rgb(46, 46, 46); padding-top: 90px; padding-bottom: 90px;">
+    
+    <div class="container">
+        <div class="row">
+            <div class="mbr-footer-content col-xs-12 col-md-3">
+                <p><strong>Address</strong><br>
+1234 Street Name<br>
+City, AA 99999<br><br><br>
+<strong>Contacts</strong><br>
+Email: support@mobirise.com<br>
+Phone: +1 (0) 000 0000 001<br>
+Fax: +1 (0) 000 0000 002</p>
+            </div>
+            <div class="mbr-footer-content col-xs-12 col-md-3"><p class="mbr-contacts__text"><strong>Links</strong></p><ul><li><a class="text-white" href="https://mobirise.com/">Website builder</a></li><li><a class="text-white" href="https://mobirise.com/mobirise-free-win.zip">Download for Windows</a></li><li><a class="text-white" href="https://mobirise.com/mobirise-free-mac.zip">Download for Mac</a></li></ul></div>
+            <div class="col-xs-12 col-md-6">
+                <div class="mbr-map"><iframe frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA0Dx_boXQiwvdz8sJHoYeZNVTdoWONYkU&amp;q=place_id:ChIJn6wOs6lZwokRLKy1iqRcoKw" allowfullscreen=""></iframe></div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+  <script src="assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="assets/tether/tether.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/jarallax/jarallax.js"></script>
+  <script src="assets/smooth-scroll/smooth-scroll.js"></script>
+  <script src="assets/viewport-checker/jquery.viewportchecker.js"></script>
+  <script src="assets/dropdown/js/script.min.js"></script>
+  <script src="assets/touch-swipe/jquery.touch-swipe.min.js"></script>
+  <script src="assets/theme/js/script.js"></script>
+  
+  
+  <input name="animation" type="hidden">
+  </body>
+</html>
